02467f30e1378b4ebbc2fd533b6c6d54a7a462ca
mekal <mekal@GDQVP7CPCVN00.lan>
2025-12-23 07:37:52 +0000

    Test case2 added

---
+++ MyProject/src/testcases/testcase2.java
@@ -0,0 +1,10 @@
+package testcases;
+
+public class testcase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println(" this is test case 2 for Github");
+	}
+
+}
