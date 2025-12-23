3dbdd4636b9f797a4adaa1f912ad7c0c45ff1b43
MAXHUB <MAXHUB@DESKTOP-0RQF8FK.bbrouter>
2025-12-23 13:11:09 +0530

    Testcase2 updated...

--- MyProject/src/testcases/testcase2.java
+++ MyProject/src/testcases/testcase2.java
@@ -4,7 +4,9 @@ public class testcase2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- System.out.println(" this is test case 2 for Github");
+		System.out.println(" this is test case 2 for Github");
+		System.out.println("TestCase 2 updated....");	
+ 
 	}
 
 }
