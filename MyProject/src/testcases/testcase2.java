62bf47236473bdd062687102cedd81a38bf157ba
mekal <mekal@GDQVP7CPCVN00.lan>
2025-12-29 20:54:10 +0000

    TestCase2  updated with ONE more today

--- MyProject/src/testcases/testcase2.java
+++ MyProject/src/testcases/testcase2.java
@@ -6,8 +6,8 @@ public class testcase2
 		// TODO Auto-generated method stub
 		System.out.println(" This is test case 2 for GitHub");
 		System.out.println("TestCase 2 updated....");	
-		
 		System.out.println("Checking updates for reference...."); 
-		System.out.println(" Added ONE More Line in TEST CASE 2 e....");  
+		
+		System.out.println(" Added ONE More Line TODAY 29/12/2025  in TEST CASE 2 e....");  
 	}
 }
